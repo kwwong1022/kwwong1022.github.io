d1096ea1181782fe180e405f8fea4c25c70e74bc
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2022-03-06 03:01:52 +0800

    v 0.0.3

---
+++ js/style.js
@@ -0,0 +1,27 @@
+// fade in effect
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -150px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(
+    function(entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) return;
+
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        })
+    }, 
+    appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+});
---
+++ p5-sketchs/assignment-7/travesty-implementation-s.js
@@ -0,0 +1,17 @@
+travestyImplementationSketch = (s) => {
+
+    s.setup = function() {
+        s.createCanvas(400, 300);
+        canvasWidth = s.width;
+    }
+
+    s.draw = function() {
+        s.background(252);
+    }
+
+    s.windowResized = function() {
+        //s.resizeCanvas(document.querySelector('#post-note-s').clientWidth, canvasHeight);
+    }
+}
+
+var travestyImplementationP5 = new p5(travestyImplementationSketch, 'travesty-implementation-s');
