33c3c0a568680fc50b64ad99f67ff586df7db4d0
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2022-03-10 20:31:58 +0800

    v 0.0.7

--- p5-sketchs/assignment-7/travesty.js
+++ p5-sketchs/assignment-7/travesty.js
@@ -1,31 +1,38 @@
-/** reference from Coding Challenge #42.1: Markov Chains - Part 1 https://www.youtube.com/watch?v=eGFJ8vugIWA */ */
+/** reference from Coding Challenge #42.1: Markov Chains - Part 1 https://www.youtube.com/watch?v=eGFJ8vugIWA */
 
 class Travesty {
-    constructor(txt, n, ngrams) {
-        this.txt = txt;
-        this.n = n;
-        this.ngrams = ngrams;
+    constructor(text, n, ngrams) {
+        this.text = text;
+        this.n = n;  // n-size mentioned in the article
+        this.ngrams = ngrams;  // the frequency table object
     }
     process() {
-        // all n-size "words"
-        for (let i=0; i<this.txt.length-this.n; i++) {
-            let gram = this.txt.substring(i, i+this.n);
+        // all n-size "words" - from start to length - n-size, make sure every word has three characters
+        for (let i=0; i<this.text.length-this.n; i++) {
+            let gram = this.text.substring(i, i+this.n);
         
+            // create new array if the pattern is new 
             if (!this.ngrams[gram]) this.ngrams[gram] = [];
-            this.ngrams[gram].push(this.txt.charAt(i+this.n));
+            // record the character after this n-size pattern -> i+this.n
+            this.ngrams[gram].push(this.text.charAt(i+this.n));
         }
     }
     generate(container) {
-        let currentGram = this.txt.substring(0, this.n);
-        let result = currentGram;
+        let curr = this.text.substring(0, this.n);
+        let result = curr;
 
-        for (let i=0; i<this.txt.length; i++) {
-            let possibilities = this.ngrams[currentGram];
+        // loop through the whole text
+        for (let i=0; i<this.text.length; i++) {
+            // get all possibilities character of that n-size pattern
+            let possibilities = this.ngrams[curr];
 
             if (possibilities) {
+                // choose one of the possibilities
                 let next = possibilities[Math.floor(Math.random()*possibilities.length)];
+                // concat it to the result to form sentence
                 result += next;
-                currentGram = result.substring(result.length-this.n, result.length);
+                //move forward to next ngram
+                curr = result.substring(result.length-this.n, result.length);
             }
         }
 
@@ -36,6 +43,7 @@ class Travesty {
             container.appendChild(p);
             container.appendChild(b);
         } else {
+            // for the api
             return result;
         }
     }
