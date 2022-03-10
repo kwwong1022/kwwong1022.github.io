c567fe6682a4428a7ca506e5b6006d1d576212b1
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2022-03-10 20:04:13 +0800

    v 0.0.6

--- p5-sketchs/assignment-7/travesty-implementation-s.js
+++ p5-sketchs/assignment-7/travesty-implementation-s.js
@@ -28,3 +28,44 @@ let init = async () => {
 }
 
 init();
+
+class Travesty {
+    constructor(txt, n, ngrams) {
+        this.txt = txt;
+        this.n = n;
+        this.ngrams = ngrams;
+    }
+    process() {
+        // all n-size "words"
+        for (let i=0; i<this.txt.length-this.n; i++) {
+            let gram = this.txt.substring(i, i+this.n);
+        
+            if (!this.ngrams[gram]) this.ngrams[gram] = [];
+            this.ngrams[gram].push(this.txt.charAt(i+this.n));
+        }
+    }
+    generate(container) {
+        let currentGram = this.txt.substring(0, this.n);
+        let result = currentGram;
+
+        for (let i=0; i<this.txt.length; i++) {
+            let possibilities = this.ngrams[currentGram];
+
+            if (possibilities) {
+                let next = possibilities[Math.floor(Math.random()*possibilities.length)];
+                result += next;
+                currentGram = result.substring(result.length-this.n, result.length);
+            }
+        }
+
+        if (container) {
+            const p = document.createElement('p');
+            const b = document.createElement('br');
+            p.append(result);
+            container.appendChild(p);
+            container.appendChild(b);
+        } else {
+            return result;
+        }
+    }
+}
